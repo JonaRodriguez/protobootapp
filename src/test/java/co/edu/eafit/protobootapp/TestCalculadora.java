57ca079fc68fc449965c2f084f4bd6d67267c174
JonaRodriguez <jonathanrodriguez0104@gmail.com>
2024-09-20 00:42:22 -0500

    Update TestCalculadora.java

--- src/test/java/co/edu/eafit/protobootapp/TestCalculadora.java
+++ src/test/java/co/edu/eafit/protobootapp/TestCalculadora.java
@@ -10,8 +10,7 @@ public class TestCalculadora {
     @Test
     public void testCuadradoPositivo() {
         Calculadora calc = new Calculadora();
-       // assertEquals(25.0, calc.cuadrado(5.0), 0);
-        return 222222;
+       assertEquals(25.0, calc.cuadrado(5.0), 0);
         //esto es una prueba
     }
 
